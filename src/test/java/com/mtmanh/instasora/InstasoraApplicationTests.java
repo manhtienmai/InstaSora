1ea3307991684cbead325f6a7971022f9de7b32e
manhtienmai <mtmanh04@gmail.com>
2025-04-15 23:07:09 +0700

    build first user service

---
+++ src/test/java/com/mtmanh/instasora/InstasoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtmanh.instasora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstasoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
